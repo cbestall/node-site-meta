22cd7da67ee2ddf5da21a5f142c19f7970c8b760
Chris Bestall <chris.bestall@gmail.com>
2015-03-10 02:20:04 +0000

    Fixed with with relative path in RSS and added test fixture

--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
+var nock = require( "nock" );
+var request = require("request");
 var SiteMeta = require( '../lib/' )(  );
 
-SiteMeta.parse( 'http://google.com', function(err, s) {console.log( s );} );
-SiteMeta.parse( 'http://tigernet.com', function(err, s) {console.log( s );} );
-SiteMeta.parse( 'https://github.com/cheeriojs/cheerio', function(err, s) {console.log( s );} );
-SiteMeta.parse( 'https://digitalocean.com/blog', function(err, s) {console.log( s );} );
+var scope = nock('https://news.ycombinator.com/')
+                .get('/')
+                // .reply(200, 'Hello from Google!');
+                .replyWithFile(200, __dirname + '/fixtures/hackernews.html');
+
+
+SiteMeta.parse( 'https://news.ycombinator.com/', function(err, s) {console.log( s );} );
+// request("https://news.ycombinator.com/",function (error, response, body) {
+  // console.log(error);
+  // console.log(response);
+//   console.log(body);
+// })
---
+++ test/test.js
@@ -0,0 +1,8 @@
+
+var SiteMeta = require( '../lib/' )(  );
+
+// SiteMeta.parse( 'http://google.com', function(err, s) {console.log( s );} );
+// SiteMeta.parse( 'http://tigernet.com', function(err, s) {console.log( s );} );
+// SiteMeta.parse( 'https://github.com/cheeriojs/cheerio', function(err, s) {console.log( s );} );
+// SiteMeta.parse( 'https://digitalocean.com/blog', function(err, s) {console.log( s );} );
+SiteMeta.parse( 'https://news.ycombinator.com/', function(err, s) {console.log( s );} );
